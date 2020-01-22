c38bf90127c153207b6a87bf9d295b89d58404cc
senha-mastertech <Elaine Cardoso>
2020-01-22 17:20:42 -0300

    adicionar melhoramentos no codigo

---
+++ src/main/java/br/com/mastertech/hometeacher/AppController.java
@@ -0,0 +1,28 @@
+package br.com.mastertech.hometeacher;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController{
+    @GetMapping
+    public String mostrarHome(){
+        return "index";
+    }
+
+    @GetMapping("/produtos")
+    public String mostrarProdutos(){
+        return"produtos";
+    }
+
+    @GetMapping("/sobre")
+    public String mostrarSobre() {
+        return "sobre";
+    }
+
+    @GetMapping("/contato")
+    public String mostrarContato() {
+        return "contato";
+    }
+}
+
